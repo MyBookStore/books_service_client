e313353eafe3137b0b50c34f1e3d6401e1a0018d
Akshay <kumarsharmaa69@gmail.com>
2016-06-19 21:46:19 +0530

    changed restclient method from "get" to "execute"

--- lib/books_service_client/base_service.rb
+++ lib/books_service_client/base_service.rb
@@ -3,8 +3,17 @@ require 'rest_client'
 module BooksServiceClient
   module BaseService
 
-    def execute url
-      RestClient.get url
+    def execute(method, url, params = {}, user_id = nil)
+      params = JSON.dump(params.merge(:user_id => user_id))
+      begin
+        response = RestClient::Request.new(
+            method: method,
+            url: url,
+            user: user_id,
+            payload: params,
+            headers: {"Content-Type" => "application/json"}).execute
+      end
+      response
     end
 
   end
